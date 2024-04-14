43229cb162e51b88e93d19a8c146f046dae18af4
ds-2024 <yoonds714@nate.com>
2024-04-14 23:00:22 +0900

    loginSuccess

--- vue-grocerystore/src/router/index.js
+++ vue-grocerystore/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import LoginFormView from '@/views/LoginFormView.vue'
+import LoginScsView from '@/views/LoginScsView.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/loginform',
     name: 'LoginFormView',
     component: LoginFormView
+  },
+  {
+    path: '/loginsuccess',
+    name: 'LoginScsView',
+    component: LoginScsView
   },
   
 ]
---
+++ vue-grocerystore/src/views/LoginScsView.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- <AppHeader /> -->
+    <!-- //header -->
+    <div class="success-container">
+      <span id="name">이연수님,</span> 
+      <span id="welcome"> 환영합니다!</span>
+        <p id="now">삼뚜기의 모든 것을 지금부터 만나보세요</p>
+        <img class="new" src="@/assets/images/new-product.png" alt="신상">
+    
+      <router-link to="/main" id="startBtn">삼뚜기몰 시작하기</router-link>
+    </div>
+    <!--<AppFooter />-->
+    <!--//footer-->
+  </template>
+  
+  <script>
+  import "@/assets/css/loginScs.css"
+  //import AppFooter from '@/components/AppFooter.vue';
+  //import AppHeader from '@/components/AppHeader.vue';
+  export default {
+      name: "LoginScsView",
+      components: {
+          //AppFooter,
+          //AppHeader
+      },
+      data() {
+          return {};
+      },
+      methods: {},
+      created() { }
+  };
+  </script>
+  
